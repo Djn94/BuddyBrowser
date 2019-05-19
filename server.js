3177be4ccc26ed49708193fa29e80045b9154573
Non Dewman <Dnewman1934@yahoo.com>
2019-05-19 16:34:05 -0500

    Server

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const path = require("path");
+const http = require("http");
+
 const express = require("express");
+const PORT = 3000;
+function handleRequest(request, response) {
+    response.end('You good, path it ' + request.url);
+}
+const server1 = http.createServer(handleRequest);
+server1.listen(PORT, function () {
+    console.log("Server listening on http://localhost:" + PORT);
+})

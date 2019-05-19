671bac5e3417ec1fcca31be4f6c24a12ef5a138c
Non Dewman <Dnewman1934@yahoo.com>
2019-05-19 16:52:27 -0500

    break

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const path = require("path");
 const http = require("http");
-
+const fs = require("fs");
 const express = require("express");
-const PORT = 3000;
-function handleRequest(request, response) {
-    response.end('You good, path it ' + request.url);
+const PORT = 8015
+function displayIndex(url, req, res) {
+    fs.readFile(__dirname + "/index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+
+        res.end(data);
+    }
+    )
 }
+
 const server1 = http.createServer(handleRequest);
 server1.listen(PORT, function () {
     console.log("Server listening on http://localhost:" + PORT);
 })
+
+function handleRequest(request, response) {
+    var path = req.url;
+    displayIndex();
+    // switch (path) {
+
+    // case "/":
+    //     return displayIndex(path, req, res);
+
+    // case "/portfolio":
+    //     return displayQuestions(path, req, res);
+
+    //     default:
+    //         return display404(path, req, res);
+    // }
+}

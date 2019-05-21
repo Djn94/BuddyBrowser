d351248b7d2409970ba35791b7c518ff7f4c2bde
Non Dewman <Dnewman1934@yahoo.com>
2019-05-21 11:55:08 -0500

    working directory

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const path = require("path");
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 const express = require("express");
-const PORT = 8015
+const PORT = 8080;
+const server = http.createServer(handleRequest);
+
+server.listen(PORT), function () {
+    console.log('server listening on: http://localhost:' + PORT);
+};
+function handleRequest(req, res) {
+    const path = req.url;
+    switch (path) {
+        case "/":
+            return displayIndex(path, req, res, res)
+        case "/FavFoods":
+            return displayQuestions(path, req, res)
+        default:
+            return displayIndex(path, req, res)
+    }
+};
 function displayIndex(url, req, res) {
     fs.readFile(__dirname + "/index.html", function (err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-
+        res.writeHead(200, {
+            "Content-Type": "text / html"
+        });
         res.end(data);
-    }
-    )
-}
-
-const server1 = http.createServer(handleRequest);
-server1.listen(PORT, function () {
-    console.log("Server listening on http://localhost:" + PORT);
-})
-
-function handleRequest(request, response) {
-    var path = req.url;
-    displayIndex();
-    // switch (path) {
-
-    // case "/":
-    //     return displayIndex(path, req, res);
-
-    // case "/portfolio":
-    //     return displayQuestions(path, req, res);
-
-    //     default:
-    //         return display404(path, req, res);
-    // }
-}
+    });
+};
+function displayQuestions(url, req, res) {
+    fs.readFile(__dirname + "/Questions.html", function (err, data) {
+        res.writeHead(200, {
+            "Content-Type": "text / html"
+        });
+        res.end(data);
+    });
+};

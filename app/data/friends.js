da30ba9fcbf5dab10d4519348ccdaad8625e15c8
Non Dewman <Dnewman1934@yahoo.com>
2019-05-23 14:16:13 -0500

    working api routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,4 +3,10 @@ var friendArray = [{
     "name": "Jeff Cash",
     "img": "here's an img url",
     "scores": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+},
+{
+    "name": "Jeff the dog",
+    "img": "here's an img url again",
+    "scores": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 }]
+module.exports = friendArray;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,9 +1,20 @@
-const friendData = require("../app/data/friends")
-
+const friendData = require("../app/data/friends.js");
+const express = require("express");
+const app = express();
+console.log('below this is the array')
+console.log(friendData)
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
+        console.log(friendData.friendArray)
         res.json(friendData);
+    });
+    app.post("/api/friends", function (req, res) {
+        console.log('api accessed')
+        console.log(friendData)
+        console.log(res.json(friendData))
+        console.log(req.body);
         friendData.push(req.body);
     });
+
 
 };
